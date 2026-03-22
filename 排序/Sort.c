38b94fadd1add0cf7ade3508f9a5bb9761b7e718
Jxcai777523 <785196815@qq.com>
2026-03-22 17:47:41 +0800

    Add files via upload

---
+++ 排序/Sort.c
@@ -0,0 +1,112 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include"Sort.h"
+
+void BubbleSort(int* arr, int n)
+{
+	for (int i = 0;i < n;i++)
+	{
+		for (int j = 0;j < n - i;j++)
+		{
+			if (arr[j - 1] > arr[j])
+			{
+				int tmp = arr[j - 1];
+				arr[j - 1] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
+}
+void InsertSort(int* arr, int n)//时间复杂度O(n^2)
+{
+	for (int i = 0;i < n - 1;i++)//这里i最多为n-2,因为要保证后面的tmp不越界
+	{
+		int end = i;
+		int tmp = arr[end + 1];
+		while (end >= 0)//从0到end都看成有序的
+		{
+			if (arr[end] > tmp)
+			{
+				arr[end + 1] = arr[end];
+				end--;
+			}
+			else
+			{
+				break;
+			}
+		}
+		arr[end+1] = tmp;//这里需要注意
+	}
+}
+void Swap(int* n1, int* n2)
+{
+	int tmp = *n1;
+	*n1 = *n2;
+	*n2 = tmp;
+}
+void BuildHeap(int* arr,int  n)
+{
+
+	for (int i = (n - 1 - 1) / 2;i >= 0;i--)
+	{
+		AdjustDown(arr, n, i);
+	}
+}
+void AdjustDown(int* arr, int size, int parent)
+{
+	//因为我们要找出左右孩子中较小的那一个进行交换
+	//我们先假设左孩子是较小的那一个
+	int child = 2 * parent + 1;
+	while (child < size)
+	{
+		if (child + 1 < size && arr[child + 1] > arr[child])
+		{
+			child++;
+		}
+		if (arr[child] > arr[parent])
+		{
+			Swap(&arr[child], &arr[parent]);
+			parent = child;
+			child = 2 * parent + 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+void HeapSort(int* arr,int n)//堆排序
+{
+	BuildHeap(arr, n);
+	int end = n - 1;
+	while (end > 0)
+	{
+		Swap(arr, &arr[end]);
+		AdjustDown(arr, end, 0);
+		end--;
+	}
+}
+void ShellSort(int* arr, int n)
+{
+	int gap = n;
+	while (gap > 1)
+	{
+		gap = gap / 3 + 1;
+		for (int i = 0;i < n - gap;i++)
+		{
+			int end = i;
+			int tmp = arr[end + gap];
+			for (end;end >= 0;end -= gap)
+			{
+				if (arr[end] > tmp)
+				{
+					arr[end + gap] = arr[end];
+				}
+				else
+				{
+					break;
+				}
+			}
+			arr[end + gap] = tmp;
+		}
+	}
+}
---
+++ 排序/test.c
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include"Sort.h"
+//void PrintArr(int* arr, int n)
+//{
+//	for (int i = 0;i < n;i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//}
+void Testspeed()
+{
+	srand(time(NULL));//先生成一个随机种子
+	const int N = 1000000;
+	int* a1 = (int*)malloc(sizeof(int) * N);
+	int* a2 = (int*)malloc(sizeof(int) * N);
+	int* a3 = (int*)malloc(sizeof(int) * N);
+	int* a4 = (int*)malloc(sizeof(int) * N);
+	int* a5 = (int*)malloc(sizeof(int) * N);
+	int* a6 = (int*)malloc(sizeof(int) * N);
+	int* a7 = (int*)malloc(sizeof(int) * N);
+	for (int i = 0;i < N;i++)
+	{
+		a1[i] = rand();
+		a2[i] = a3[i] = a4[i] = a5[i] = a6[i] = a7[i] = a1[i];
+	}
+	/*int begin1 = clock();
+	BubbleSort(a1, N);
+	int end1 = clock();
+	printf("BubbleSort:%d\n", end1 - begin1);*/
+
+	int begin2 = clock();
+	InsertSort(a1, N);
+	int end2 = clock();
+	printf("InsertSort:%d\n", end2 - begin2);
+
+	int begin3 = clock();
+	HeapSort(a1, N);
+	int end3 = clock();
+	printf("HeapSort: %d\n", end3 - begin3);
+
+	int begin4 = clock();
+	ShellSort(a1, N);
+	int end4 = clock();
+	printf("ShellSort: %d\n", end4 - begin4);
+}
+int main()
+{
+	int arr[] = { 9,8,7,6,5,4,3,2,1 };
+	//BubbleSort(arr, 9);
+	//InsertSort(arr, 9);
+	Testspeed();
+	return 0;
+}
